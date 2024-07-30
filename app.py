13b80d83ed9af4eee566deefdbf300501e438350
Gabriel da Mota <gabrielmotaa@hotmail.com>
2024-07-30 10:19:04 -0300

    Alerta inicial e HTMX

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,3 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/alert')
+def alert():
+    level = random.choice(['info', 'error', 'success', 'warning'])
+    return f'''
+    <div class="alert is-{level}">
+      <strong>{level.upper()}</strong> Esse é um alerta vindo do Flask!
+    </div>'''
