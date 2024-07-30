41add6b5bc3772ac193bae5fdcc8194be836ee56
Gabriel da Mota <gabrielmotaa@hotmail.com>
2024-07-30 10:21:58 -0300

    Adição de middleware incial

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 import random
 import json
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, session, flash
 
 app = Flask(__name__)
+app.secret_key = '<super-secret>'
+
+
+@app.after_request
+def flash_to_htmx_trigger(response):
+    """Middleware que converte flashes para HX-Trigger."""
+    flashes = session.pop('_flashes') if '_flashes' in session else []
+
+    if not flashes:
+        return response
+
+    trigger_content = {'alerts': flashes}
+    response.headers['HX-Trigger'] = json.dumps(trigger_content)
+
+    return response
 
 
 @app.route('/')
@@ -13,8 +28,9 @@ def index():
 @app.route('/alert')
 def alert():
     level = random.choice(['info', 'error', 'success', 'warning'])
-    response = make_response()
-    response.headers['HX-Trigger'] = json.dumps({
-      'alerts': [('info', 'Esse é um alerta vindo do HX-Trigger!')]
-    })
-    return response
+    flash('Esse é um alerta vindo do HX-Trigger!', level)
+
+    # No caso só queremos o alerta, mas temos que retornar um corpo vazio para
+    # ser uma função de rota válida. Agora 'flash' pode ser chamado em qualquer
+    # rota e um alerta sempre vai ser criado com a mensagem.
+    return ''
